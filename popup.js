fd08e3806ddc7e66a7e85f953d7322df1cb9d3ff
hiroyamochi <hirodora13@gmail.com>
2024-02-08 19:44:41 +0900

    Close popup windows after saving

--- popup.js
+++ popup.js
@@ -3,5 +3,6 @@ document.getElementById('save').addEventListener('click', function() {
   var buttonNumber = document.getElementById('buttonNumber').value;
   chrome.storage.sync.set({selectedButtonNumber: buttonNumber}, function() {
     console.log('設定が保存されました');
+    window.close();
   });
 });
